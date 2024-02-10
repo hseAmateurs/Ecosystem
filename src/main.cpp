09cf9d8e530760d3f0de8561c09c0efa441a2ad5
Shon Qwezon <shonzbso@gmail.com>
2024-02-10 20:16:10 +0300

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+    // Простой пример для проверки работы
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
