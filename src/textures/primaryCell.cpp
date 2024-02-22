55c12b4ed979fb6272f71e9238e2427e0188873a
bukhva <iabukhvalov@edu.hse.ru>
2024-02-22 15:46:34 +0300

    add filling of textures

--- src/textures/primaryCell.cpp
+++ src/textures/primaryCell.cpp
@@ -23,7 +23,7 @@ void texture::PrimaryCell::updatePulsationAspect() {
 }
 
 texture::PrimaryCell::PrimaryCell(sf::Vector2f center, float radius, int pointsCount, sf::Color color) :
-        center(center), radius(radius), m_vertices(sf::LineStrip, pointsCount + 1), pointsCount(pointsCount),
+        center(center), radius(radius), m_vertices(sf::TriangleFan, pointsCount + 2), pointsCount(pointsCount),
         color(color) {
     parameters = {
             10,
@@ -41,7 +41,11 @@ void texture::PrimaryCell::update() {
 
     float angle = 0, step = (float)(2 * M_PI / pointsCount);
     float offset;
-    for (int i = 0; i <= pointsCount; ++i) {
+
+    // look at sf::TriangleFan
+    m_vertices[0].position = center;
+    m_vertices[0].color = color;
+    for (int i = 1; i <= pointsCount+1; ++i) {
         sf::Vector2f currentRadiusVector = getRadiusVector(angle, radius);
         offset = getOffset(angle);
         currentRadiusVector *= (1.f + offset);
