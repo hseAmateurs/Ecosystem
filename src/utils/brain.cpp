71907f5843169454d9a2e119b80f0394997ae91a
Shon Qwezon <shonzbso@gmail.com>
2024-03-11 18:33:58 +0300

    Add funcs for handling brain animation

---
+++ src/utils/brain.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Shon on 11.03.2024.
+//
+
+#include "brain.h"
+#include <cmath>
+
+using namespace settings;
+
+namespace brain {
+    void updateAngle(Animation &anim, const sf::Clock &timer) {
+        double deltaAngle = anim.targetAngle - anim.currentAngle;
+        double scale = sin(M_PI * timer.getElapsedTime().asSeconds() / (anim.speedScale / anim.speed));
+        anim.currentAngle += deltaAngle * scale;
+        if (anim.currentAngle > anim.targetAngle) anim.currentAngle = anim.targetAngle;
+    }
+
+    sf::Vector2f getXY(const double angle, const double distance) {
+        float posX = std::cos(angle) * (distance * BRAIN_RADIUS);
+        float posY = std::sin(angle) * (distance * BRAIN_RADIUS);
+        return {SCREEN_WIDTH - posX, SCREEN_HEIGHT - posY};
+    }
+
+    sf::Vector2f getXY(const int index, const int amount, const double distance) {
+        const double angleOffset = M_PI / 8;
+        double angle = (M_PI / 2 + angleOffset) / (amount + 1) * (amount - index) - angleOffset / 2;
+        return getXY(angle, distance);
+    }
+}
---
+++ src/utils/brain.h
@@ -0,0 +1,27 @@
+//
+// Created by Shon on 11.03.2024.
+//
+
+#ifndef ECOSYSTEM_BRAIN_H
+#define ECOSYSTEM_BRAIN_H
+
+#include <SFML/Graphics.hpp>
+#include "settings.h"
+
+
+namespace brain {
+    struct Animation {
+        double targetAngle;
+        double currentAngle;
+        double speedScale;
+        double speed;
+    };
+
+    sf::Vector2f getXY(int index, int amount, double distance = settings::BCELL_DISTANCE);
+
+    sf::Vector2f getXY(double angle, double distance = settings::BCELL_DISTANCE);
+
+    void updateAngle(Animation &anim, const sf::Clock &timer);
+}
+
+#endif //ECOSYSTEM_BRAIN_H
