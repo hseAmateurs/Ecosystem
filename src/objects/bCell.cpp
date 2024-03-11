8edec1ee70288da4b708c26bfc7b0dc2e01cebd6
Shon Qwezon <shonzbso@gmail.com>
2024-03-11 18:37:32 +0300

    Move anim func to brain.h

--- src/objects/bCell.cpp
+++ src/objects/bCell.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include "bCell.h"
 
+using namespace brain;
+
 void BCell::drawTexture(sf::RenderWindow &window, sf::Time elapsed) {
     texture.changeCenter(getPosition());
     texture.update(elapsed);
@@ -18,36 +20,38 @@ BCell::~BCell() {
     std::cout << "Deleted\n";
 }
 
-void BCell::updateAngle() {
-    double deltaAngle = anim.targetAngle - anim.currentAngle;
-    double scale = sin(M_PI * timer.getElapsedTime().asSeconds() / (anim.speedScale / speed));
-    anim.currentAngle += deltaAngle * scale;
-    if(anim.currentAngle > anim.targetAngle) anim.currentAngle = anim.targetAngle;
-}
-
-
 void BCell::update(Field &field, sf::Time deltaTime) {
     if (m_status == SCROLL) {
-        int index = getID(field);
-        sf::Vector2f nextPos = getXY(index - 1, field.bCells.size() - 1);
-        sf::Vector2f curPos = getXY(index, field.bCells.size() - 1);
-        anim = {
-                atan2(SCREEN_HEIGHT - nextPos.y, SCREEN_WIDTH - nextPos.x),
-                atan2(SCREEN_HEIGHT - curPos.y, SCREEN_WIDTH - curPos.x),
-                600 + (double)(rand() % 1000 - 500)
-        };
-        m_status = MOVING;
-        timer.restart();
+        scrollPrepare(field);
         return;
     }
-    if(m_status == MOVING) {
-        updateAngle();
+    if (m_status == MOVING) {
+        updateAngle(anim, timer);
         setPosition(getXY(anim.currentAngle));
-        if(anim.currentAngle >= anim.targetAngle) {
+        if (anim.currentAngle >= anim.targetAngle) {
+            int index = getID(field);
+            if(index == 0) {
+                field.bCells.erase(field.bCells.begin());
+                delete this;
+            }
             m_status = FREE;
         }
         return;
     }
+}
+
+void BCell::scrollPrepare(const Field &field) {
+    int index = getID(field);
+    sf::Vector2f nextPos = getXY(index - 1, field.bCells.size() - 1);
+    sf::Vector2f curPos = getXY(index, field.bCells.size() - 1);
+    anim = {
+            atan2(SCREEN_HEIGHT - nextPos.y, SCREEN_WIDTH - nextPos.x),
+            atan2(SCREEN_HEIGHT - curPos.y, SCREEN_WIDTH - curPos.x),
+            600 + (double)(rand() % 1000 - 500),
+            speed
+    };
+    m_status = MOVING;
+    timer.restart();
 }
 
 int BCell::getID(const Field &field) const {
@@ -56,23 +60,4 @@ int BCell::getID(const Field &field) const {
         if (cell == this) return index;
         index++;
     }
-}
-
-
-sf::Vector2f BCell::getXY(const double angle, const double distance) {
-    float posX = std::cos(angle) * (distance * BRAIN_RADIUS);
-    float posY = std::sin(angle) * (distance * BRAIN_RADIUS);
-    return {SCREEN_WIDTH - posX, SCREEN_HEIGHT - posY};
-}
-
-sf::Vector2f BCell::getXY(const int index, const int amount) {
-    const double angleOffset = M_PI / 8;
-    double angle = (M_PI / 2 + angleOffset) / (amount + 1) * (amount - index) - angleOffset / 2;
-    return getXY(angle);
-}
-
-sf::Vector2f BCell::nextOrbitXY(const int index, const int amount) {
-    const double angleOffset = M_PI / 8;
-    double angle = (M_PI / 2 + angleOffset) / (amount + 1) * (amount - index) - angleOffset / 2;
-    return getXY(angle, MACRO_DISTANCE);
 }
